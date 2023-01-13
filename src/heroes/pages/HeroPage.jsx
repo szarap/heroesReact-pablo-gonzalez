9335d5fdce25be47b8b77e1c4c8496e616af6c2a
Pablo Gonzalez <lic.szgonzalez@gmail.com>
2023-01-13 05:18:22 -0300

    cambios en la ruta de imagen hero Page

--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -23,7 +23,7 @@ export const HeroPage = () => {
     <div className="row mt-5">
       <div className="col-4">
         <img 
-          src={ `/assets/heroes/${ id }.jpg` } 
+          src={ `/heroes/${ id }.jpg` } 
           alt={ hero.superhero }
           className="img-thumbnail animate__animated animate__fadeInLeft"
         />
